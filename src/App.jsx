fbcac9ca49fd39261a7f5da6fd62eb1e958c99ac
salysaly <anyawee.sr@gmail.com>
2024-02-20 21:07:43 +0700

    <NewProjectForm />

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ProjectDetail from "./components/ProjectDetail";
+import YourProject from "./components/YourProjects";
+
+const App = () => {
+  return (
+    <>
+      <YourProject />
+      <ProjectDetail />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/YourProjects.jsx
@@ -0,0 +1,14 @@
+const YourProject = () => {
+  return (
+    <section className="bg-slate-950 w-80 mt-10 inset-y-0 left-0 rounded-r-lg text-left">
+      <h2 className="text-white ml-6 mt-20 pb-4 uppercase font-bold">
+        YOUR PROJECT
+      </h2>
+      <button className="ml-6 mt-2 px-4 py-2 bg-stone-600 rounded-lg text-slate-200 font-normal">
+        + Add Project
+      </button>
+    </section>
+  );
+};
+
+export default YourProject;
